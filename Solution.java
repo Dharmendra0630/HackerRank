61b0c4c790d78b880c9f6d2261a1d568a3a3ba06
Dharmendra <Dharmendra@192.168.1.4>
2017-02-22 10:54:39 +0530

    Hacker Rank Java Loops I commit

---
+++ Solution.java
@@ -0,0 +1,22 @@
+package com.main.hacker_rank.loops; 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = 0;
+        System.out.println("enter the value 2");
+        int N = in.nextInt();
+        for(int i=1;i<=10;i++){
+        	 a=N*i;
+             System.out.print(N+" x "+i+" = " );
+             System.out.println(a);
+        }
+       
+    }
+}
